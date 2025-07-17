3c328469f9eaf2731434a28dc25ca1387dcc7f6f
rigoloberto <145296949+rigoloberto@users.noreply.github.com>
2025-07-17 08:34:04 -0500

    running at localhost
    dejado para que por dejecto corra con localhost y no la web

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 esto configura de donde saco el backend al dar ngserve o ngbuild
 cuando se hace ngserve o ngbuild, se usa el archivo environment.ts
 cuando se hace ng build --configuration production, se usa el archivo environment.prod.ts
+*/
 
 export const environment = {
   production: false,
@@ -13,8 +14,8 @@ export const authapi = {
   production: false,
   apiUrl: 'http://localhost:3000/api/auth'
 };
-*/
 
+/*
 export const environment = {
   production: true,
   apiUrl: 'https://sistema-adopcion-backend.onrender.com/api'
@@ -24,3 +25,4 @@ export const authapi = {
   production: true,
   apiUrl: 'https://sistema-adopcion-backend.onrender.com/api/auth'
 };
+*/
